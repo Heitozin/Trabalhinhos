21f76dbaf708cef3c52ab287143a083aa5e090fe
Heitô <98980899+Heitozin@users.noreply.github.com>
2022-11-04 22:07:40 -0300

    mercadinho

---
+++ trabalhosHeitor/mercadinho/JSmercadinho.js
@@ -0,0 +1,46 @@
+let form = document.getElementById("form-notas");
+let btn = document.getElementById("gerar");
+let relatorio = document.getElementById("relatorio")
+//Vetor = Array
+let produtos = [];
+let quantidades = [];
+let valores = [];
+
+
+//Váriavel auxiliar
+let mostrarRel = "";
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    //push - adicionar novos dados dentro de um vetor array
+    produtos.push(form.produto.value);
+    quantidades.push(Number(form.quantidade.value));
+    valores.push(Number(form.valor.value));
+   
+  
+
+
+})
+
+btn.addEventListener('click', function(event){
+    event.preventDefault();
+    mostrarRel = "";
+
+    let valor_total = 0
+    
+    
+    for(let posicao = 0; posicao < produtos.length; posicao++){
+        if(quantidades[posicao] > 1){
+        valor_total = valores[posicao] * quantidades[posicao]
+    }
+
+        /*descVr = salarios[posicao] * 0.09;
+        descVt = salarios[posicao] * 0.06;
+        salarioLiquido = salarios[posicao] - descVt - descVr;*/
+
+        mostrarRel += `<p>Produto: ${produtos[posicao]} | Quantidade de produtos: ${quantidades[posicao]} | Valor total: ${valor_total}` 
+    }
+    relatorio.innerHTML = "";
+    relatorio.innerHTML = mostrarRel;
+})
